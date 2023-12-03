c80380fed52a3c6c465b0eed6d57ae4c8958536d
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-12-04 01:21:50 +0200

    updated the urls

--- src/thekomp.js
+++ src/thekomp.js
@@ -1,5 +1,4 @@
 // const api = "https://server.bwengeserver.com";
-// const api = "http://13.59.38.98:5000";
-const api = "http://13.59.38.98:5000/";
+const api = "http://13.59.38.98:5000";
 
 export default api;
